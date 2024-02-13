d696c1791946e0f52df00e0566a4e03300a66b10
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-02-13 15:32:48 -0300

    --fix Adding verification if the elements do exist.

--- src/modules/GeraSenha.js
+++ src/modules/GeraSenha.js
@@ -1,43 +1,49 @@
 //GERA A SENHA
 //MÓDULO DEPENDENTE DE DEFINE CARACTERES
-import DefineCaracteres from './DefineCaracteres'
+import DefineCaracteres from "./DefineCaracteres";
 
 class GeraSenha {
-    //usa todos os métodos para gerar a senha
-    static geraSenha(){
-        let allowed = DefineCaracteres.defineCaract()
-        let senha = ''
+  //usa todos os métodos para gerar a senha
+  static geraSenha() {
+    let allowed = DefineCaracteres.defineCaract();
+    let senha = "";
 
-        senha = GeraSenha.includesNecessary(allowed, senha)
-        senha = GeraSenha.includesQuantity(allowed, senha, 20)
-        GeraSenha.mostraSenha(senha)
+    senha = GeraSenha.includesNecessary(allowed, senha);
+    senha = GeraSenha.includesQuantity(allowed, senha, 20);
+    GeraSenha.mostraSenha(senha);
+  }
+
+  //inclui pelo menos um dos caracteres necessários
+  static includesNecessary(allowed, senha) {
+    allowed.forEach((item, index) => {
+      senha = `${senha}${item[Math.floor(Math.random() * item.length)]}`;
+    });
+    return senha;
+  }
+
+  //inclui a quantidade de caracteres necessários
+  static includesQuantity(allowed, senha, necCaract) {
+    while (senha.length < necCaract) {
+      let arrayIndex = Math.floor(Math.random() * allowed.length);
+      let stringIndex = Math.floor(Math.random() * allowed[arrayIndex].length);
+      senha = `${senha}${allowed[arrayIndex][stringIndex]}`;
     }
 
-    //inclui pelo menos um dos caracteres necessários
-    static includesNecessary(allowed, senha){
-        allowed.forEach((item, index) => {
-            senha = `${senha}${item[(Math.floor(Math.random() * item.length))]}` 
-        })
-        return senha
-    }
+    return senha;
+  }
 
-    //inclui a quantidade de caracteres necessários
-    static includesQuantity(allowed, senha, necCaract){
-        while (senha.length < necCaract){
-            let arrayIndex = Math.floor(Math.random() * allowed.length)
-            let stringIndex = Math.floor(Math.random() * allowed[arrayIndex].length)
-            senha = `${senha}${allowed[arrayIndex][stringIndex]}`
-        }
-
-        return senha
+  //mostra senha na tela
+  static mostraSenha(senha) {
+    if(document.querySelector(".novaSenha")){
+        document.querySelector(".novaSenha").innerHTML = `${senha}`;
     }
-
-    //mostra senha na tela
-    static mostraSenha(senha){
-        document.querySelector('.novaSenha').innerHTML = `${senha}`
-    }
+  }
 }
 
-document.querySelector('.gerar-senha').addEventListener('click', () => {
-    GeraSenha.geraSenha()
-})
+if (document.querySelector(".gerar-senha")) {
+  document.querySelector(".gerar-senha").addEventListener("click", () => {
+    GeraSenha.geraSenha();
+  });
+} else {
+    console.error('Botão de gerar senha não existe.')
+}
